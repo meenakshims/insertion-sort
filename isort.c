041111558272dce87d6032dc2aafa02ec5c4ca7c
MEENAKSHI M S <45414@user>
2017-06-23 17:17:02 +0530

    added read fn again

--- isort.c
+++ isort.c
@@ -16,6 +16,16 @@ int sort(int *array,int size)
   return 0;	  
 }
 
+int read(int *array)
+{
+  int i,size;
+  scanf("%d",&size);
+  for(i=0;i<size;i++)
+    {
+      scanf("%d",&array[i]);
+    }
+  return size;
+}
 
 int main()
 {
